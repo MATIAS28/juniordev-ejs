5db2f43f803e6fd15e1bd561a2a6c455da126022
MATIAS28 <38082435+MATIAS28@users.noreply.github.com>
2023-03-27 13:04:21 -0300

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,9 +1,8 @@
 'use strict'
-
-var mongoose = require('mongoose');
 //DB conection
 var mongoose = require('mongoose');
 const MONGODB_URI = process.env.MONGODB_URI
+console.log(MONGODB_URI)
 mongoose.Promise = global.Promise;
 mongoose.connect(MONGODB_URI, { connectWithNoPrimary: true, useNewUrlParser: true,  useUnifiedTopology: true })
                 .then(() => {
