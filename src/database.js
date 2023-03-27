f0efbfbe148a7250fd204d93be9dc79344535f22
MATIAS28 <38082435+MATIAS28@users.noreply.github.com>
2023-03-27 13:56:15 -0300

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,10 +1,8 @@
 'use strict'
 //DB conection
-var mongoose = require('mongoose');
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://Matmun:Mocorillo123@kobdb-ybabt.mongodb.net/KOBdb?retryWrites=true&w=majority'
-console.log(MONGODB_URI)
+var mongoose = require('mongoose')
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGODB_URI, { connectWithNoPrimary: true, useNewUrlParser: true,  useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://Matmun:Mocorillo123@kobdb-ybabt.mongodb.net/KOBdb?retryWrites=true&w=majority', { connectWithNoPrimary: true, useNewUrlParser: true,  useUnifiedTopology: true })
                 .then(() => {
                   console.log('Conectado a la base de datos');
                 })
